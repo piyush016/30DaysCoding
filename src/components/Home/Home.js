71e04a8bf0d961519f5a72a8c5aa0a6e866370e3
Piyush <piyushcool116@gmail.com>
2022-08-08 10:57:43 +0530

    Development: Home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,24 +1,12 @@
 import React from "react";
 import background from "../../assets/lim.jpg";
 import "./style.css";
-import { Link } from "react-router-dom";
-import { Button } from "react-bootstrap";
 
 export default function Home() {
   return (
     <>
       <div>
         <img className="img" alt="background" src={background} />
-      </div>
-      <div className="login">
-        <Link to="/login">
-           <Button>Login</Button>
-        </Link>
-      </div>
-      <div className="signup">
-        <Link to="/signup">
-         
-        </Link>
       </div>
     </>
   );
